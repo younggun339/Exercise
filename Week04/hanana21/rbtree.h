4230e09ca52d9e5613e418940df7d51f6dc1a1d0
이하나 <134573117+hanana21@users.noreply.github.com>
2024-02-07 12:47:26 +0900

    Create rbtree.h

---
+++ Week04/hanana21/rbtree.h
@@ -0,0 +1,39 @@
+#ifndef _RBTREE_H_
+#define _RBTREE_H_
+
+#include <stddef.h>
+
+typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
+
+typedef int key_t;
+
+typedef struct node_t {
+  color_t color;
+  key_t key;
+  struct node_t *parent, *left, *right;
+} node_t;
+
+typedef struct {
+  node_t *root;
+  node_t *nil;  // for sentinel
+} rbtree;
+
+rbtree *new_rbtree(void);
+void delete_rbtree(rbtree *);
+
+node_t* get_next_node(const rbtree* t, node_t *p);
+void exchange_color(node_t* a, node_t* b);
+void rbtree_insert_fixup(rbtree* t, node_t* Node);
+void right_rotate(rbtree* t, node_t* Node);
+void left_rotate(rbtree* t, node_t* Node);
+node_t* get_next_node(const rbtree* t, node_t *p);
+
+node_t *rbtree_insert(rbtree *, const key_t);
+node_t *rbtree_find(const rbtree *, const key_t);
+node_t *rbtree_min(const rbtree *);
+node_t *rbtree_max(const rbtree *);
+int rbtree_erase(rbtree *, node_t *);
+
+int rbtree_to_array(const rbtree *, key_t *, const size_t);
+
+#endif  // _RBTREE_H_
